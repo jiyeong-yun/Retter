74a1116d358358c1d8e29b0be8065db0a01887c0
김혜인 <fallingstars2749@gmail.com>
2022-03-09 00:29:30 +0900

    Feat: add Card, Message component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
-import Card from "./routes/Card";
+import Card from "./routes/card/Card";
+import Message from "./routes/card/Message";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/card" element={<Card />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<Message />}></Route>
+        <Route path="/card/edit" element={<Card />}></Route>
       </Routes>
     </Router>
   );
--- frontend/src/routes/Card.js
+++
@@ -1,3 +0,0 @@
-export default function Card() {
-  return <div>카드 페이지</div>;
-}
