5de09a026ab4ae0edb0fd4a658143b117d03f03c
성당현 <1003dang@naver.com>
2022-03-08 18:20:10 +0900

    Feat: add Detail.js

--- frontend/src/routes/Detail.js
+++ frontend/src/routes/Detail.js
@@ -1,6 +1,18 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LineShareButton,
+  LineIcon,
+} from "react-share";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 function Detail() {
   // 클릭하면 페이지이동
   function handleClick(e) {
@@ -22,6 +34,22 @@ function Detail() {
     <div>
       <h2>메세지결과</h2>
       <button onClick={handleClick}>처음으로</button>
+      <br />
+      <FacebookShareButton style={{ marginRight: "20px" }} url={`https://localhost3000/result/${id}`}>
+        <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+      </FacebookShareButton>
+      <FacebookMessengerShareButton style={{ marginRight: "20px" }} url={`https://localhost3000/result/${id}`}>
+        <FacebookMessengerIcon size={48} round={true} borderRadius={24}></FacebookMessengerIcon>
+      </FacebookMessengerShareButton>
+      <TwitterShareButton style={{ marginRight: "20px" }} url={`https://localhost3000/result/${id}`}>
+        <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+      </TwitterShareButton>
+      <LineShareButton style={{ marginRight: "20px" }} url={`https://localhost3000/result/${id}`}>
+        <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+      </LineShareButton>
+      <CopyToClipboard text={`https://localhost3000/result/${id}`}>
+        <button>url</button>
+      </CopyToClipboard>
     </div>
   );
 }
