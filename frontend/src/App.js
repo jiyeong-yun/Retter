6833615e4969bee1369875eda313483021b4972c
성당현 <1003dang@naver.com>
2022-03-12 02:05:33 +0900

    Refactor: Arrow Function & import setAppElement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import Home from "./routes/Home";
 import Card from "./routes/card/Card";
 import Message from "./routes/card/Message";
 import Detail from "./routes/Detail";
+import ReactModal from 'react-modal';
+
+// index.js에 써도 차이 없음
+ReactModal.setAppElement('#root');
 
 function App() {
   return (
@@ -11,9 +15,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/card">
           <Route path="" element={<Message />} />
-          <Route path="edit" element={<Card />}></Route>
+          <Route path="edit" element={<Card />} />
         </Route>
-        <Route path="/result/:id" element={<Detail />}></Route>
+        <Route path="/result/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
