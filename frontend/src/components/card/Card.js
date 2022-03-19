1c3a8370a566d9c5d65509f82d38f16e6edeaf1b
김혜인 <fallingstars2749@gmail.com>
2022-03-19 19:55:06 +0900

    [S06P22C202-46] Feat: drag stickers

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -1,5 +1,7 @@
+import { useRef } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import { setStickerPos } from "../../store/actions/cardActions";
 
 function mapStateToProps({ cardReducer }) {
   return {
@@ -8,12 +10,64 @@ function mapStateToProps({ cardReducer }) {
     text: cardReducer.text,
   };
 }
-export default connect(mapStateToProps)(CardComponent);
-function CardComponent({ background, stickers, text }) {
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setStickerPos: (index, x, y) => dispatch(setStickerPos(index, x, y)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardComponent);
+
+function CardComponent({ background, stickers, text, setStickerPos }) {
+  const card = useRef();
+  let pageX, pageY;
+  const handleDragStart = (event) => {
+    // event.preventDefault();
+    event.target.style.cursor = "grab";
+    // 커서 위치를 구하기 위해 이동 전 좌표를 저장해둠
+    pageX = event.pageX;
+    pageY = event.pageY;
+  };
+
+  const handleDrag = (event) => {
+    event.target.style.cursor = "grab";
+  };
+
+  const handleDragEnd = (event, index) => {
+    const cardRef = card.current.getBoundingClientRect();
+    const target = event.target.getBoundingClientRect();
+
+    // 마우스 커서 위치를 기억
+    const shiftX = pageX - target.left;
+    const shiftY = pageY - target.top;
+    // 다음 좌표 갱신
+    let nextX = event.pageX - cardRef.left - shiftX;
+    let nextY = event.pageY - cardRef.top - shiftY;
+
+    // 카드 영역 밖으로는 나가지 못하도록 함
+    if (nextX < 0) nextX = 0;
+    if (nextY < 0) nextY = 0;
+    if (nextX > cardRef.width - target.width)
+      nextX = cardRef.width - target.width;
+    if (nextY > cardRef.height - target.height)
+      nextY = cardRef.height - target.height;
+
+    setStickerPos(index, nextX, nextY);
+    event.target.style.cursor = "grab";
+  };
+
   return (
-    <Card color={background.color}>
+    <Card color={background.color} ref={card}>
       {stickers.map((sticker, index) => (
-        <Sticker key={index} sticker={sticker}>
+        <Sticker
+          key={index}
+          sticker={sticker}
+          draggable
+          onDragStart={(event) => handleDragStart(event, index)}
+          onDrag={(event) => handleDrag(event)}
+          onDragEnd={(event) => handleDragEnd(event, index)}
+        >
           {sticker.id}
         </Sticker>
       ))}
@@ -27,6 +81,7 @@ const Card = styled.section`
   position: relative;
   width: 300px;
   height: 300px;
+  overflow: hidden;
 `;
 
 const Sticker = styled.div`
@@ -36,4 +91,9 @@ const Sticker = styled.div`
   left: ${(props) => props.sticker.x}px;
   width: ${(props) => props.sticker.width}px;
   height: ${(props) => props.sticker.height}px;
+  background-color: orange;
+
+  &:hover {
+    cursor: grab;
+  }
 `;
