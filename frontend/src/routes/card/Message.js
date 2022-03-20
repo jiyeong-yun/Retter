c76131be3859ae7f0b67f5d9d045e226aec06953
김혜인 <fallingstars2749@gmail.com>
2022-03-20 13:59:25 +0900

    [S06P22C202-71] Feat: filter message

--- frontend/src/routes/card/Message.js
+++ frontend/src/routes/card/Message.js
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import { connect } from "react-redux";
 import { setMessage } from "../../store/actions/cardActions";
+import { useNavigate } from "react-router-dom";
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -12,13 +12,33 @@ function mapDispatchToProps(dispatch) {
 export default connect(null, mapDispatchToProps)(Message);
 
 function Message({ setMessage }) {
-  const [text, setText] = useState();
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
   const handleChange = ({ target: { value } }) => {
     setText(value);
   };
 
   const handleClick = () => {
-    setMessage(text);
+    if (text === '') {
+      alert("메세지를 입력해주세요!");
+      return;
+    }
+
+    // 1. 영어 삭제
+    // 아래 전처리된 문장이 서버로 보내지고, 사용자에게는 이 텍스트로만 보임
+    const nextText = text.replace(/\w/g, '');
+    setMessage(nextText);
+    console.log(nextText);
+
+    // 2. 서버로 전송할 메세지 전처리
+    let preprocessedText = nextText.replace(/[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣|0-9|.?! ]/g, '');
+    // 온점, 느낌표, 물음표 1개만 남김
+    preprocessedText = preprocessedText.replace(/\.+/g, '.');
+    preprocessedText = preprocessedText.replace(/!+/g, '!');
+    preprocessedText = preprocessedText.replace(/\?+/g, '?');
+
+    // todo: 서버로 메세지 보내는 로직 
+    navigate('/card/edit');
   };
 
   return (
@@ -30,9 +50,10 @@ function Message({ setMessage }) {
         onChange={handleChange}
         placeholder="메세지를 입력해주세요!"
       ></textarea>
-      <Link to="/card/edit">
-        <button onClick={handleClick}>다음</button>
-      </Link>
+      <ul>
+        <li>Re:tter는 한글, 숫자만 지원해요. 영어 멈춰!! (메세지에 영어가 들어가면 자동으로 삭제됩니다.)</li>
+      </ul>
+      <button onClick={handleClick}>다음</button>
     </main>
   );
 }
