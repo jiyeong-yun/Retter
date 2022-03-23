d1188c0b55ca142590e307fdab1741fe2fa05873
SSAFY <kys980130@naver.com>
2022-03-23 14:46:06 +0900

    Feat: create test:

--- backend/card/serializers.py
+++ backend/card/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Card
 
 class CardSerializer(serializers.HyperlinkedModelSerializer):
-    image = serializers.ImageField(use_url=True)
-    audio = serializers.FileField(use_url=True)
+    image = serializers.ImageField(use_url=True, required=False)
+    audio = serializers.FileField(use_url=True, required=False)
 
     class Meta:
         model = Card
