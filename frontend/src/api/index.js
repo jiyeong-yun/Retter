4d7de8149385f41fe6e2993a282b26d68c467144
김혜인 <fallingstars2749@gmail.com>
2022-03-28 14:00:26 +0900

    Fix: Modified api address
    Former-commit-id: 9d40b39d2d36764f1722dfaebf75009685bffb16

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://127.0.0.1:8000/";
+const BACKEND_URL = "http://127.0.0.1:8000/api";
 
 function apiInstance() {
   const instance = axios.create({
