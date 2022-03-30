610dee09e2fce49fb1d5f68f451560b129e33745
김혜인 <fallingstars2749@gmail.com>
2022-03-30 13:26:19 +0900

    Feat: Add production URL
    Former-commit-id: 2d10a2283d17fb809cb4ad559990b00307a69959

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://127.0.0.1:8000/api";
+const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://j6c202.p.ssafy.io/api"
+    : "http://127.0.0.1:8000/api";
 
 function apiInstance() {
   const instance = axios.create({
