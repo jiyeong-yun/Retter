597d0f1b8b8f4c863eafda7e4bd709e69876aaf9
김혜인 <fallingstars2749@gmail.com>
2022-04-01 14:08:54 +0900

    [S06P22C202-113] Feat: Send image to Server
    Former-commit-id: 3dbc65da51f8417f7fe4eab51bbbed0cbf973812

--- frontend/src/components/card/MenuBar.js
+++ frontend/src/components/card/MenuBar.js
@@ -4,7 +4,6 @@ import html2canvas from "html2canvas";
 import { useCallback } from "react";
 import { connect } from "react-redux";
 import { sendImageURL } from "../../api/message";
-import axios from "axios";
 
 function mapStateToProps({ cardReducer }) {
   return {
@@ -17,55 +16,35 @@ function Menu({ card_id }) {
   const saveCard = useCallback(() => {
     const card = document.getElementById("card");
     html2canvas(card).then(function (canvas) {
-      // const url = canvas.toDataURL();
-      // console.log(card_id);
       canvas.toBlob(function (blob) {
-        const url = URL.createObjectURL(blob);
+        const file = new File([blob], "card.png", {
+          lastModified: new Date().getTime(),
+          type: "image/png",
+        });
 
         const form = new FormData();
-        form.append("image", url);
-        // const params = {
-        //   card_id,
-        //   form,
-        // };
-        // sendImageURL(
-        //   params,
-        //   (response) => console.log(response),
-        //   (error) => console.log(error)
-        // );
-
-        axios
-          .post(`http://127.0.0.1:8000/api/card/${card_id}/`, form, {
-            headers: { "Content-Type": "multipart/form-data" },
-          })
-          .then((response) => console.log(response))
-          .catch((error) => console.log(error));
-      });
-
-      // todo: 서버로 이미지 보내기
-
-      // console.log(params);
-      // sendImageURL(
-      //   params,
-      //   (response) => console.log(response),
-      //   (error) => console.log(error)
-      // );
-
-      // axios
-      //   .post(`http://127.0.0.1:8000/api/card/${card_id}/`, form, {
-      //     headers: { "Content-Type": "multipart/form-data" },
-      //   })
-      //   .then((response) => console.log(response))
-      //   .catch((error) => console.log(error));
+        form.append("image", file);
+        const params = {
+          card_id,
+          form,
+        };
+        sendImageURL(
+          params,
+          (response) => console.log(response),
+          (error) => console.log(error)
+        );
+      }, "image/png");
     });
   }, [card_id]);
+
+  const deleteCard = useCallback(() => {}, []);
 
   return (
     <nav>
       <ul>
         <List onClick={saveCard}>저장</List>
         <List>재생</List>
-        <List>
+        <List onClick={deleteCard}>
           <ArrowBackIosRoundedIcon />
         </List>
       </ul>
